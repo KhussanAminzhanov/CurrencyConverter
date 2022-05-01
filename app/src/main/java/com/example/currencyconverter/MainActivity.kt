13839542b6ca2c6eef6965af5c3322c8f4c3cbdf
Khussan Aminzhanov <aminjanovhusan07@gmail.com>
2022-05-01 19:09:03 +0500

    minor refactor

--- app/src/main/java/com/example/currencyconverter/MainActivity.kt
+++ app/src/main/java/com/example/currencyconverter/MainActivity.kt
@@ -31,12 +31,11 @@ class MainActivity : AppCompatActivity(), Animation.AnimationListener {
         val buttonBack: Button = findViewById(R.id.buttonBack)
         val buttonOk: Button = findViewById(R.id.buttonOk)
         val buttonIndices = intArrayOf(
-            R.id.button0, R.id.button1, R.id.button2,
-            R.id.button3, R.id.button4, R.id.button5,
-            R.id.button6, R.id.button7, R.id.button8,
-            R.id.button9,
+            R.id.button0, R.id.button1, R.id.button2, R.id.button3, R.id.button4,
+            R.id.button5, R.id.button6, R.id.button7, R.id.button8, R.id.button9,
         )
 
+        
         textView.text = updatePinCodeText(0)
 
         for (i in 0..9) {
