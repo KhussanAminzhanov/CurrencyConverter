93a0371c0ca0d30a08798cb6a31298bdfc3e9bde
Khussan Aminzhanov <aminjanovhusan07@gmail.com>
2022-05-25 16:45:33 +0600

    add UserProfileFragment.kt

---
+++ app/src/main/java/com/example/currencyconverter/userprofilescreen/UserProfileFragment.kt
@@ -0,0 +1,68 @@
+package com.example.currencyconverter.userprofilescreen
+
+import android.content.Intent
+import android.graphics.Bitmap
+import android.net.Uri
+import android.os.Bundle
+import android.provider.MediaStore
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.appcompat.app.AppCompatActivity
+import androidx.fragment.app.Fragment
+import com.example.currencyconverter.databinding.FragmentUserProfileBinding
+
+const val REQUEST_IMAGE_CAPTURE = 1
+
+class UserProfileFragment : Fragment() {
+
+    private var _binding: FragmentUserProfileBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding = FragmentUserProfileBinding.inflate(inflater, container, false)
+        val view = binding.root
+
+        binding.userName.text = "User Name"
+        binding.userEmail.text = "example@mail.com"
+
+        binding.shareButton.setOnClickListener {
+            val shareIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, "Message of share button")
+                type = "text/plain"
+            }
+            val intent = Intent.createChooser(shareIntent, "Share with")
+            startActivity(intent)
+        }
+
+        binding.shareEmailButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_SENDTO)
+            intent.data = Uri.parse("mailto:example@mail.com")
+            startActivity(Intent.createChooser(intent, "Send with"))
+        }
+
+        binding.callButton.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel: +7747627929"))
+            startActivity(intent)
+        }
+
+        binding.openCameraButton.setOnClickListener {
+            val intent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivityForResult(intent, REQUEST_IMAGE_CAPTURE)
+        }
+        return view
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_IMAGE_CAPTURE && resultCode == AppCompatActivity.RESULT_OK) {
+            val imageBitmap = data?.extras?.get("data") as Bitmap
+            binding.userIcon.setImageBitmap(imageBitmap)
+        }
+    }
+
+}
