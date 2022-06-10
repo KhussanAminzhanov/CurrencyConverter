b477f96c6f67f333df1900825504bda668243439
Khussan Aminzhanov <aminjanovhusan07@gmail.com>
2022-06-10 15:58:01 +0600

    [ADDED] fragment_movie_info.xml and MovieInfoFragment.kt

--- app/src/main/java/com/example/currencyconverter/favoritesscreen/FavoritesAdapter.kt
+++ app/src/main/java/com/example/currencyconverter/favoritesscreen/FavoritesAdapter.kt
@@ -2,23 +2,26 @@ package com.example.currencyconverter.favoritesscreen
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.fragment.app.FragmentManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.currencyconverter.R
 import com.example.currencyconverter.databinding.ItemPodcastPlayerBinding
 import com.example.currencyconverter.databinding.ItemVideoPlayerBinding
 
-class FavoritesAdapter(list: List<String>) : RecyclerView.Adapter<FavoritesViewHolder>() {
-
-    private val data = list
+class FavoritesAdapter(
+    private val data: List<String>,
+    private val fm: FragmentManager
+) : RecyclerView.Adapter<FavoritesViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): FavoritesViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         return when (viewType) {
             TYPE_VIDEO -> {
-                val binding = ItemVideoPlayerBinding.inflate(inflater)
+                val binding = ItemVideoPlayerBinding.inflate(inflater, parent, false)
                 FavoritesViewHolder(binding)
             }
             else -> {
-                val binding = ItemPodcastPlayerBinding.inflate(inflater)
+                val binding = ItemPodcastPlayerBinding.inflate(inflater, parent, false)
                 FavoritesViewHolder(binding)
             }
         }
@@ -29,6 +32,11 @@ class FavoritesAdapter(list: List<String>) : RecyclerView.Adapter<FavoritesViewH
         when (holder.itemViewType) {
             TYPE_VIDEO -> {
                 val binding = holder.videoBinding
+
+                binding?.root?.setOnClickListener {
+                    fm.beginTransaction().add(R.id.fcv_favorites_content, MovieInfoFragment())
+                        .commit()
+                }
             }
             else -> {
                 val binding = holder.podcastBinding
@@ -39,12 +47,12 @@ class FavoritesAdapter(list: List<String>) : RecyclerView.Adapter<FavoritesViewH
     override fun getItemCount() = data.size
 
     override fun getItemViewType(position: Int): Int {
-        return if (position % 2 == 0) TYPE_PODCAST
+        return if ((position + 1) % 2 == 0) TYPE_PODCAST
         else TYPE_VIDEO
     }
 
     companion object {
         const val TYPE_VIDEO = 0
-        const val TYPE_PODCAST = 0
+        const val TYPE_PODCAST = 1
     }
 }
--- app/src/main/java/com/example/currencyconverter/favoritesscreen/FavoritesFragment.kt
+++ app/src/main/java/com/example/currencyconverter/favoritesscreen/FavoritesFragment.kt
@@ -1,11 +1,11 @@
 package com.example.currencyconverter.favoritesscreen
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.currencyconverter.R
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
 import com.example.currencyconverter.databinding.FragmentFavoritesBinding
 
 class FavoritesFragment : Fragment() {
@@ -13,11 +13,19 @@ class FavoritesFragment : Fragment() {
     private var _binding: FragmentFavoritesBinding? = null
     val binding get() = _binding!!
 
+    private lateinit var adapter: FavoritesAdapter
+    private lateinit var fm: FragmentManager
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
         _binding = FragmentFavoritesBinding.inflate(inflater)
+        fm = requireActivity().supportFragmentManager
+
+        adapter = FavoritesAdapter(listOf("First", "Second", "Third", "Fourth", "Fifth", "Sixth"), fm)
+        binding.rvNews.adapter = adapter
+
         return binding.root
     }
 
